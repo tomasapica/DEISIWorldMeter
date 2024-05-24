8ead60ec00296940c83277871e798ac5f486db83
tomaspica <167481630+tomasapica@users.noreply.github.com>
2024-05-24 01:34:23 +0100

    Add files via upload

---
+++ src/Cidades.java
@@ -0,0 +1,29 @@
+package pt.ulusofona.aed.deisiworldmeter;
+
+public class Cidades {
+    String alfa2;
+    String cidade;
+    String regiao;
+    int populacao;
+    double latitude;
+    double longitude;
+
+    public Cidades(String alfa2, String cidade, String regiao, int populacao, double latitude, double longitude) {
+        this.alfa2 = alfa2;
+        this.cidade = cidade;
+        this.regiao = regiao;
+        this.populacao = populacao;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Cidades() {
+
+    }
+
+    @Override
+    public String toString(){
+        return cidade + " | " + alfa2.toUpperCase() + " | " + regiao + " | " +
+                populacao + " | " + "(" + latitude + "," + longitude + ")";
+    }
+}
